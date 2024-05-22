30226c68f8fe0ea8964b6cb66235549a8e37caeb
Kevin-Caldwell <kevinc2004@gmail.com>
2024-05-22 17:06:57 -0400

    Initial Commit and Simulation is WIP

--- energy_sim.py
+++ energy_sim.py
@@ -1,161 +1,37 @@
-import math
-import sys
-import copy
-import numpy as np
 import geolib as gl
 import enlib as el
 import matplotlib.pyplot as plt
 import shapely.geometry as shp
 import random
 import sim_helper
-import min_span_tree
 
-def energy_delivery_points(delivery_pt0:int, delivery_pt1:int):
-    """djikstra's algorithm implemented between 2 delivery points"""
-    global delivery_indices, map_adj_list
-
-
-    return dijkstras(
-        map_adj_list, 
-        delivery_nodes[delivery_pt0], 
-        delivery_nodes[delivery_pt1], 
-        energy_map_points, 
-        calc_path=False
-    )
-
-def dijkstras(adjacency_list: list, start: int, dest: int, energy, calc_path=True) -> list:
-    cardinality = len(adjacency_list)
-    distances = np.ones(cardinality) * np.inf
-    distances[start] = 0
-
-    connections = np.ones(shape=(cardinality), dtype=int) * -1
-    check_stack = [start]
-    visited = np.zeros(shape=(cardinality), dtype=bool)
-
-    while check_stack:
-        n0 = check_stack.pop()
-
-        if not visited[n0]:
-            visited[n0] = True
-
-            for n1 in adjacency_list[n0]:
-                test_distance = distances[n0] + energy(n0, n1)
-
-                if test_distance < distances[n1]:
-                    distances[n1] = test_distance
-                    connections[n1] = n0
-                
-                check_stack.append(n1)
-    if calc_path:
-        path = [dest]
-        while path[0] != start or len(path) < len(adjacency_list):
-            if(connections[path[0]] == -1):
-                break
-            path.insert(0, connections[path[0]])
-        return distances[dest], path
-    else:
-        return distances[dest]
-
-def euclidean_distance(vector_a, vector_b):
-    """Returns the Euclidean Distance between vectors v1 and v2"""
-    d0 = vector_a[0] - vector_b[0]
-    d1 = vector_a[1] - vector_b[1]
-    return math.sqrt(d0 * d0 + (d1 * d1))
-
-def energy_map_points(map_vertex0:int, map_vertex1:int) -> float:
-    """Energy Required to travel on an edge on map"""
-    global map_nodes
-    n0 = map_nodes[map_vertex0]
-    n1 = map_nodes[map_vertex1]
-
-    return euclidean_distance(n0, n1)
-
-# For Repeatable Runs
-seed = random.randrange(sys.maxsize)
-random.seed(seed)
-print("Random Seed: ", seed)
-
-# GEOLIB
 PLACE_NAME = "University of Toronto"
-TARGET_CRS_EPSG = "EPSG:3348" #EPSG:3348" # Canadian EPSG
+TARGET_CRS_EPSG = "EPSG:3348" # Canadian EPSG
 BOUNDARY_BUFFER_LENGTH = 500  # default boundary buffer
-TYPE = "drive" # "drive"
+TYPE = "walk" # "drive"
 
-nodes, edges, UID_to_ind, ind_to_UID = gl.get_decomposed_network(PLACE_NAME, TARGET_CRS_EPSG, BOUNDARY_BUFFER_LENGTH, TYPE, 
+nodes, edges = gl.get_decomposed_network(PLACE_NAME, TARGET_CRS_EPSG, BOUNDARY_BUFFER_LENGTH, TYPE, 
                                          safety_check=True, simplification_tolerance=5)
 
 eh = el.EnergyHelper(nodes, edges, 10**(-2), gen_plot_data=True)
-adj_list = sim_helper.convert_to_adj_list(nodes, edges)
-map_node_count = len(nodes)
 
-#------ SIM CODE -------#
 
-# Generate an abstraction layer for delivery points
-delivery_point_count = 3
+node_count = max(nodes.keys())
+vertices = [0] * node_count
+for i in range(node_count):
+    vertices[i] = nodes[i]
+
+delivery_point_count = 10
 delivery_indices = []
 
 for i in range(delivery_point_count):
-    delivery_indices.append(random.randrange(0, map_node_count))
-print(delivery_indices)
-# (Global) Problem-Related Variables
-map_nodes = nodes
-map_adj_list = adj_list
+    delivery_indices.append(random.randrange(0, node_count))
 
-# A list where each value corresponds to the map coordinate of each 
-# delivery point.
-delivery_nodes = delivery_indices
-delivery_adj_list = []
-make_symmetric = True
+# print(delivery_indices)
+# print(edges)
 
-# print(map_adj_list)
-if make_symmetric:
-    for i, adj_nodes in enumerate(map_adj_list):
-        for adj in adj_nodes:
-            if i not in map_adj_list[adj]:
-                map_adj_list[adj].append(i)
-# print(map_adj_list)
+print(sim_helper.convert_to_adj_list(eh))
 
-
-
-# Adjacency List for delivery points, we assume every delivery point 
-# can be accessed by every other delivery point.
-for i in range(delivery_point_count):
-    adj = []
-    for j in range(delivery_point_count):
-        adj.append(j)
-    delivery_adj_list.append(adj)
-
-# Starting Vertex
-warehouse = 0
-
-# Generate Distance Matrix for delivery layer
-distance_matrix = np.zeros(shape=(delivery_point_count, delivery_point_count))
-
-for i in range(delivery_point_count):
-    for j in range(delivery_point_count):
-        distance_matrix[i, j] = energy_delivery_points(i, j)
-
-print(distance_matrix)
-# Solve GTSP using distance matrix
-from python_tsp.heuristics import solve_tsp_simulated_annealing as tsp_solve
-tour, i = tsp_solve(distance_matrix, warehouse)
-
-print(tour, i)
-tour.append(warehouse) # Fix formatting
-map_tour = []
-for node in tour:
-    map_tour.append(delivery_nodes[node])
-print(map_tour)
-    
-
-# Plotting
-sim_helper.plot_graph_adj(nodes, adj_list)
-sim_helper.plot_tour(nodes, map_tour, delivery_point_count)
-
-for i in range(delivery_point_count):
-    cost, path = dijkstras(map_adj_list, map_tour[i], map_tour[i+1], energy_map_points)
-    sim_helper.plot_tour(nodes, path, len(path)-1, edge_color='red', node_color='lightcoral')
-
+eh.plot_network()
 plt.show()
-
 
--- sim_helper.py
+++ sim_helper.py
@@ -1,40 +1,30 @@
 from enlib import EnergyHelper
-import matplotlib.pyplot as plt
 
 def sample_energy_fcn(vert1, vert2):
     pass
 
-def convert_to_adj_list(nodes, edges):
-    adj_list = []
-    for edge_list in edges:
-        adj = []
-        for adjacent_node in edge_list:
-            adj.append(adjacent_node[0])
-        adj_list.append(adj)
-    return adj_list
+def convert_to_adj_list(graph: EnergyHelper):
+    edges = graph.edges
+    vertices = graph.nodes
 
-def plot_tour(nodes, tour, point_count, edge_color='green', node_color='green'):
+    point_count = max(edges.keys())
+    adj_matrix = []
+
+    verts = [*vertices.values()]
+
     for i in range(point_count):
-        n0 = nodes[tour[i]]
-        n1 = nodes[tour[i+1]]
+        connected = edges[i]
+        adj = []
+        for edge in connected:
+            try:
+                edge_index = verts.index(edge)
+                adj.append(edge_index)
+            except(ValueError):
+                pass
 
-        x = [n0[0], n1[0]]
-        y = [n0[1], n1[1]]
+        adj_matrix.append(adj)
 
-        plt.plot(x, y, color=edge_color)
-        plt.scatter(x, y, color=node_color)
+        # print(adj)
+    return adj_matrix
 
-def plot_graph_adj(nodes, adj_list):
-    x=[]
-    y=[]
-    for p in nodes:
-        x.append(p[0])
-        y.append(p[1])
-    plt.scatter(x, y, color='silver')
 
-    for edge_list in enumerate(adj_list):
-        for edge in edge_list[1]:
-            x = [nodes[edge_list[0]][0], nodes[edge][0]]
-            y = [nodes[edge_list[0]][1], nodes[edge][1]]
-            plt.plot(x, y, 'lightgrey')
-    # plt.show()
