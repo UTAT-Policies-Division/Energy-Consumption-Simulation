9d77e9dbaa5a05b9253ad66e2aa54ac036ef0341
Kevin-Caldwell <kevinc2004@gmail.com>
2024-05-22 17:06:57 -0400

    Initial Commit and Simulation is WIP

--- sim_helper.py
+++ sim_helper.py
@@ -1,5 +1,4 @@
 from enlib import EnergyHelper
-import matplotlib.pyplot as plt
 
 def sample_energy_fcn(vert1, vert2):
     pass
@@ -8,10 +7,10 @@ def convert_to_adj_list(graph: EnergyHelper):
     edges = graph.edges
     vertices = graph.nodes
 
-    point_count = len(vertices)
+    point_count = max(edges.keys())
     adj_matrix = []
 
-    verts = vertices
+    verts = [*vertices.values()]
 
     for i in range(point_count):
         connected = edges[i]
@@ -29,28 +28,3 @@ def convert_to_adj_list(graph: EnergyHelper):
     return adj_matrix
 
 
-def plot_graph_adj(nodes, adj_list):
-    x=[]
-    y=[]
-    for p in nodes:
-        x.append(p[0])
-        y.append(p[1])
-    plt.scatter(x, y, color='silver')
-
-    for edge_list in enumerate(adj_list):
-        for edge in edge_list[1]:
-            x = [nodes[edge_list[0]][0], nodes[edge][0]]
-            y = [nodes[edge_list[0]][1], nodes[edge][1]]
-            plt.plot(x, y, 'lightgrey')
-    # plt.show()
-
-def plot_tour(nodes, tour, point_count, edge_color='green', node_color='green'):
-    for i in range(point_count):
-        n0 = nodes[tour[i]]
-        n1 = nodes[tour[i+1]]
-
-        x = [n0[0], n1[0]]
-        y = [n0[1], n1[1]]
-
-        plt.plot(x, y, color=edge_color)
-        plt.scatter(x, y, color=node_color)
