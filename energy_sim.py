cdcb6d541805367d6dbc8424a5f54d1d0f5911f6
Kevin-Caldwell <kevinc2004@gmail.com>
2024-06-10 13:27:51 -0400

    minor progress update

--- energy_sim.py
+++ energy_sim.py
@@ -19,10 +19,11 @@ def energy_delivery_points(delivery_pt0:int, delivery_pt1:int):
         map_adj_list, 
         delivery_nodes[delivery_pt0], 
         delivery_nodes[delivery_pt1], 
-        energy_map_points
+        energy_map_points, 
+        calc_path=False
     )
 
-def dijkstras(adjacency_list: list, start: int, dest: int, energy) -> list:
+def dijkstras(adjacency_list: list, start: int, dest: int, energy, calc_path=True) -> list:
     cardinality = len(adjacency_list)
     distances = np.ones(cardinality) * np.inf
     distances[start] = 0
@@ -45,13 +46,15 @@ def dijkstras(adjacency_list: list, start: int, dest: int, energy) -> list:
                     connections[n1] = n0
                 
                 check_stack.append(n1)
-    
-    path = [dest]
-    while path[0] != start or len(path) < len(adjacency_list):
-        if(connections[path[0]] == -1):
-            break
-        path.insert(0, connections[path[0]])
-    return distances[dest], path
+    if calc_path:
+        path = [dest]
+        while path[0] != start or len(path) < len(adjacency_list):
+            if(connections[path[0]] == -1):
+                break
+            path.insert(0, connections[path[0]])
+        return distances[dest], path
+    else:
+        return distances[dest]
 
 def euclidean_distance(vector_a, vector_b):
     """Returns the Euclidean Distance between vectors v1 and v2"""
@@ -73,7 +76,7 @@ random.seed(seed)
 print("Random Seed: ", seed)
 
 # GEOLIB
-PLACE_NAME = "Toronto"
+PLACE_NAME = "University of Toronto"
 TARGET_CRS_EPSG = "EPSG:3348" #EPSG:3348" # Canadian EPSG
 BOUNDARY_BUFFER_LENGTH = 500  # default boundary buffer
 TYPE = "drive" # "drive"
@@ -85,10 +88,10 @@ eh = el.EnergyHelper(nodes, edges, 10**(-2), gen_plot_data=True)
 adj_list = sim_helper.convert_to_adj_list(nodes, edges)
 map_node_count = len(nodes)
 
-#------SIM CODE-------#
+#------ SIM CODE -------#
 
 # Generate an abstraction layer for delivery points
-delivery_point_count = 10
+delivery_point_count = 3
 delivery_indices = []
 
 for i in range(delivery_point_count):
@@ -102,6 +105,17 @@ map_adj_list = adj_list
 # delivery point.
 delivery_nodes = delivery_indices
 delivery_adj_list = []
+make_symmetric = True
+
+# print(map_adj_list)
+if make_symmetric:
+    for i, adj_nodes in enumerate(map_adj_list):
+        for adj in adj_nodes:
+            if i not in map_adj_list[adj]:
+                map_adj_list[adj].append(i)
+# print(map_adj_list)
+
+
 
 # Adjacency List for delivery points, we assume every delivery point 
 # can be accessed by every other delivery point.
@@ -119,10 +133,11 @@ distance_matrix = np.zeros(shape=(delivery_point_count, delivery_point_count))
 
 for i in range(delivery_point_count):
     for j in range(delivery_point_count):
-        distance_matrix[i, j], _ = energy_delivery_points(i, j)
+        distance_matrix[i, j] = energy_delivery_points(i, j)
 
+print(distance_matrix)
 # Solve GTSP using distance matrix
-from python_tsp.heuristics import solve_tsp_record_to_record as tsp_solve
+from python_tsp.heuristics import solve_tsp_simulated_annealing as tsp_solve
 tour, i = tsp_solve(distance_matrix, warehouse)
 
 print(tour, i)
@@ -139,7 +154,6 @@ sim_helper.plot_tour(nodes, map_tour, delivery_point_count)
 
 for i in range(delivery_point_count):
     cost, path = dijkstras(map_adj_list, map_tour[i], map_tour[i+1], energy_map_points)
-    # print((map_tour[i], map_tour[i+1]), (cost, path))
     sim_helper.plot_tour(nodes, path, len(path)-1, edge_color='red', node_color='lightcoral')
 
 plt.show()
